f078ffa477266225fbe952cbf1f464718fc36b28
Raguerrebere17 <raguerrebere17@gmail.com>
2023-02-23 01:25:56 -0800

    6-print_line is now fixed

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,19 +7,19 @@
 
 void print_line(int n)
 {
-int a = 0;
+	int a = 0;
 
-if (n > 0)
-{
-while (a <= n)
-{
-_putchar('_');
-a++;
-}
-_putchar('\n');
-}
-else
-{
-_putchar('\n');
-}
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			_putchar('_');
+			a++;
+		}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
