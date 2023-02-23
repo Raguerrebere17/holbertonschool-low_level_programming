60d664dc018d6916b77233c62803ad86a280b080
Raguerrebere17 <raguerrebere17@gmail.com>
2023-02-23 02:48:08 -0800

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else
+		{
+		printf("%d ", a);
+		}
+	}
+	return (0);
+}
