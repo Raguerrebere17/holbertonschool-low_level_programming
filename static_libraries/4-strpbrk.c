efc3d48068c6e76ae9035549f3af2d7cc9c82378
Raguerrebere17 <raguerrebere17@gmail.com>
2023-03-10 19:08:58 -0800

    Task 1 completed

---
+++ static_libraries/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: argument received
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+}
