2d757a29e9e23341b66a501178708caf129cd3be
Raguerrebere17 <raguerrebere17@gmail.com>
2023-03-18 13:18:57 -0700

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+int
+**alloc_grid(int width, int height)
+{
+	int i = 0;
+	int a = 0;
+	int **x;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	qdl = (int **)malloc(height * sizeof(int *));
+	if (x == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		x[i] = (int *)malloc(sizeof(int) * width);
+		if (x[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(x[i]);
+				i--;
+			}
+			free(x);
+			return (NULL);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		for (a = 0; a < width; a++)
+			x[i][a] = 0;
+		i++;
+	}
+	return (x);
+}
