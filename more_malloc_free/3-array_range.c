842209548e579b4739a90ef5c886f6aaf4dfb7ed
Raguerrebere17 <raguerrebere17@gmail.com>
2023-03-19 17:48:53 -0700

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int
+*array_range(int min, int max)
+{
+	int *p;
+	int cont;
+
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == NULL)
+		return (NULL);
+	for (cont = 0; cont < (max - min +1); cont++)
+		p[cont] = cont + min;
+	return (p);
+}
