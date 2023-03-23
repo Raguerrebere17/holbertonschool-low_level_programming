2d699403b6bfb679ccdc7519eea787cf23bea295
Raguerrebere17 <raguerrebere17@gmail.com>
2023-03-22 17:49:26 -0700

    First task and README

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef Poppy
+#define Poppy
+
+/**
+ * struct dog - Dog's info
+ * @name: Dog's Name
+ * @age: Dog's Age
+ * @owner: Dog's Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
