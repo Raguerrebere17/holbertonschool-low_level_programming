a5cc4c746146f8914ee01558baadb4e880285259
Raguerrebere17 <raguerrebere17@gmail.com>
2023-03-26 12:55:33 -0700

    Betty coding style fixed

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,23 +8,23 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	  	{"+", op_add},
+		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	};
-	    int i = 0;
+	}
+       	int i = 0;
 
-	    while (ops->op && *(ops[i].op) != *s)
-	    {
+	while (ops->op && *(ops[i].op) != *s)
+	{
 	    	i++;
-	    }
-	    if (*(ops[i].op) == *s && strlen(s) == strlen(ops[i].op))
-	    {
+	}
+	if (*(ops[i].op) == *s && strlen(s) == strlen(ops[i].op))
+	{
 	    	return (ops[i].f);
-	    }
-	    printf("Error\n");
-	    exit(99);
+	}
+	printf("Error\n");
+	exit(99);
 }
