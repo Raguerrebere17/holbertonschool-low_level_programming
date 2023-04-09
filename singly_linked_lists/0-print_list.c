b5e2cda770f5d91cd347aaf077c9db85ad4cdea9
Raguerrebere17 <raguerrebere17@gmail.com>
2023-04-09 14:10:41 -0700

    First task completed

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements
+ * @h: Pointer to the structure
+ * Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	for (; h; i++)
+	{
+		if(!h->str)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (i);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Readme that I'll complete later
