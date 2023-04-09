9e48b9d241c4f05e0f1916933a9808e4cfaf0292
Raguerrebere17 <raguerrebere17@gmail.com>
2023-04-09 14:19:57 -0700

    First & second task are now completed

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - Counts the number of elements
+ * @h: Pointer to the structure
+ * Return: The number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	for (; h; i++)
+	{
+		h = h->next:
+	}
+	return (i);
+}
