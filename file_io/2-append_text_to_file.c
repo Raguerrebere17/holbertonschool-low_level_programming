3712d97fdcb73ab33c15cf098107145717997ebe
Raguerrebere17 <raguerrebere17@gmail.com>
2023-04-13 01:27:54 -0700

    Task number 2 is now completed

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,25 +1,30 @@
 #include "main.h"
-/**
- * append_text_to_file - Write at the end of the file
- * @filename: The name of the file
- * @text_content: The content of the text
- * Return: 1 On success, -1 on Failure
- */
+#include <stdio.h>
+#include <stdlib.h>
 
-int append_text_to_file(const char *filename, char text_content)
+/**
+ * append_text_to_file - write an the end of the file.
+ *
+ * @filename: the name of the textfile.
+ * @text_content: content of text.
+ * Return: 1 on success and -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fnd, count;
+	int fnd, cont;
 
 	if (filename == NULL || !*filename)
+	{
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
+	}
+	fnd = open(filename, O_RDWR | O_APPEND);
 	if (fnd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (count = 0; *(text_content + count); count++)
-			;
-		write(fnd, text_content, count);
+		for (cont = 0; *(text_content + cont); cont++)
+		;
+		write(fnd, text_content, cont);
 	}
 	close(fnd);
 	return (1);
