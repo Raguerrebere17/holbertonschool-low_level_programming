9c906d668c15b70808fda07fdaf91968ee3b25ca
Raguerrebere17 <raguerrebere17@gmail.com>
2023-04-13 01:12:50 -0700

    Task number 2 is now completed

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - Write at the end of the file
+ * @filename: The name of the file
+ * @text_content: The content of the text
+ * Return: 1 On success, -1 on Failure
+ */
+
+int append_text_to_file(const char *filename, char text_content)
+{
+	int fnd, count;
+
+	if (filename == NULL || !*filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fnd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		for (count = 0; *(text_content + count); count++)
+			;
+		write(fnd, text_content, count);
+	}
+	close(fnd);
+	return (1);
+}
